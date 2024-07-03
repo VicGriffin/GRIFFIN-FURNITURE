c8c30b3fd3fb034f807849bd3085b2e98931df8e
vicgriffin123 <vickamworkpro@gmail.com>
2024-07-04 01:06:57 +0300

    create the stores page

--- Griffin's_furniture/src/Pages/products/Products.jsx
+++ Griffin's_furniture/src/Pages/products/Products.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 function Products() {
   return (
-    <div>Products Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur suscipit, eius maiores consequatur nisi error laborum mollitia ab voluptates accusantium.</div>
+    <div>Products</div>
   )
 }
 
--- Griffin's_furniture/src/Pages/store/Store.jsx
+++ Griffin's_furniture/src/Pages/store/Store.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './store.css';
+import S1_img from '../../assets/stores-img/billiani.jpeg'
+import S2_img from '../../assets/stores-img/kisumu.jpeg'
+import S3_img from '../../assets/stores-img/mombasa.jpeg'
+import S4_img from '../../assets/stores-img/mountke.jpeg'
+import S5_img from '../../assets/stores-img/nairobi.jpeg'
 
 function Store() {
   return (
-    <div>Store</div>
-  )
+    <>
+      <div className='heading'>
+        <h1>Most Popular Stores in Kenya</h1>
+      </div>
+      <section className='card-name'>
+        <div className="card-section">
+          <div className="card-body">
+            <img src={S2_img} alt="Kisumu Store" />
+            <div className="info">
+              <h3>Kisumu Store</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam enim delectus eligendi sit doloribus. Magni consequatur harum voluptate perferendis tempora necessitatibus aliquid. Eos natus corporis laudantium autem voluptates id eius?</p>
+            </div>
+          </div>
+        </div>
+        <div className="card-section">
+          <div className="card-body">
+            <img src={S1_img} alt="Nakuru Store" />
+            <div className="info">
+              <h3>Billiani store</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam enim delectus eligendi sit doloribus. Magni consequatur harum voluptate perferendis tempora necessitatibus aliquid. Eos natus corporis laudantium autem voluptates id eius?</p>
+            </div>
+          </div>
+        </div>
+        <div className="card-section">
+          <div className="card-body">
+            <img src={S3_img} alt="Mombasa Store" />
+            <div className="info">
+              <h3>Mombasa Store</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam enim delectus eligendi sit doloribus. Magni consequatur harum voluptate perferendis tempora necessitatibus aliquid. Eos natus corporis laudantium autem voluptates id eius?</p>
+            </div>
+          </div>
+        </div>
+        <div className="card-section">
+          <div className="card-body">
+            <img src={S5_img} alt="Nairobi Store" />
+            <div className="info">
+              <h3>Nairobi Store</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam enim delectus eligendi sit doloribus. Magni consequatur harum voluptate perferendis tempora necessitatibus aliquid. Eos natus corporis laudantium autem voluptates id eius?</p>
+            </div>
+          </div>
+        </div>
+        <div className="card-section">
+          <div className="card-body">
+            <img src={S4_img} alt="Mount Kenya Store" />
+            <div className="info">
+              <h3>Mount Kenya Store</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam enim delectus eligendi sit doloribus. Magni consequatur harum voluptate perferendis tempora necessitatibus aliquid. Eos natus corporis laudantium autem voluptates id eius?</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Store
+export default Store;
